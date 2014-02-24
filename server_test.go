a5192655b3043d5affe0123f453d60510a69cb98
unknown <goldsmithd@PCMS-LAPTOP152.pcmsgroup.com>
2014-02-24 13:29:06 +0000

    Test: Added test for Requesting Leases Out Of Local Range.

--- server_test.go
+++ server_test.go
@@ -1,9 +1,12 @@
 package dhcp4server
 
 import (
+	//"bytes"
 	"encoding/json"
 	"github.com/d2g/dhcp4"
+	"github.com/d2g/dhcp4client"
 	"github.com/d2g/dhcp4server/leasepool/memorypool"
+	"github.com/d2g/hardwareaddr"
 	"log"
 	"net"
 	"testing"
@@ -22,6 +25,9 @@ func ExampleServer() {
 
 	//Replace this section with the subnet
 	myConfiguration.SubnetMask = net.IPv4(255, 255, 255, 0)
+
+	//Default Gateway
+	myConfiguration.DefaultGateway = net.IPv4(192, 168, 1, 201)
 
 	//Update these with your selected DNS Servers these belong to OpenDNS
 	myConfiguration.DNSServers = []net.IP{net.IPv4(208, 67, 222, 222), net.IPv4(208, 67, 220, 220)}
@@ -95,4 +101,103 @@ func TestConfigurationJSONMarshalling(test *testing.T) {
 	}
 
 	test.Logf("Configuration Object:%v\n", endConfiguration)
+}
+
+/*
+ * Test Discovering a Lease That's not Within Our Lease Range.
+ * This Happens When a devce switches network.
+ * Example: Mobile Phone on Mobile internet Has IP 100.123.123.123 Switch To Home Wifi
+ * The device requests 100.123.123.123 on Home Wifi which is out of range...
+ */
+func TestDiscoverOutOfRangeLease(test *testing.T) {
+	//Start Our Example Server
+	go ExampleServer()
+
+	time.Sleep(time.Duration(5) * time.Second)
+
+	//Generate Hardware Address
+	HardwareMACAddress, err := hardwareaddr.GenerateEUI48()
+	if err != nil {
+		test.Error("Error: Can't Generate Valid MACAddress" + err.Error())
+	}
+
+	HardwareMACAddress, err = net.ParseMAC("58-94-6B-73-57-0C")
+	if err != nil {
+		log.Printf("MAC Error:%v\n", err)
+	}
+
+	//Lets Be A Client
+	client := dhcp4client.Client{
+		MACAddress:    HardwareMACAddress,
+		IgnoreServers: []net.IP{},
+		Timeout:       (time.Second * 5),
+	}
+
+	err = client.Connect()
+	defer client.Close()
+
+	if err != nil {
+		test.Error("Conection Error:" + err.Error())
+	}
+
+	discoveryPacket := client.DiscoverPacket()
+	discoveryPacket.SetCIAddr(net.IPv4(100, 102, 96, 123))
+	discoveryPacket.PadToMinSize()
+
+	err = client.SendPacket(discoveryPacket)
+	if err != nil {
+		test.Error("Error: Sending Discover Packet" + err.Error())
+	}
+
+	test.Log("--Discovery Packet--")
+	test.Logf("Client IP : %v\n", discoveryPacket.CIAddr().String())
+	test.Logf("Your IP   : %v\n", discoveryPacket.YIAddr().String())
+	test.Logf("Server IP : %v\n", discoveryPacket.SIAddr().String())
+	test.Logf("Gateway IP: %v\n", discoveryPacket.GIAddr().String())
+	test.Logf("Client Mac: %v\n", discoveryPacket.CHAddr().String())
+
+	if !bytes.Equal(discoveryPacket.CHAddr(), HardwareMACAddress) {
+		test.Error("MACAddresses Don't Match??")
+	}
+
+	offerPacket, err := client.GetOffer(&discoveryPacket)
+	if err != nil {
+		test.Error("Error Getting Offer:" + err.Error())
+	}
+
+	test.Log("--Offer Packet--")
+	test.Logf("Client IP : %v\n", offerPacket.CIAddr().String())
+	test.Logf("Your IP   : %v\n", offerPacket.YIAddr().String())
+	test.Logf("Server IP : %v\n", offerPacket.SIAddr().String())
+	test.Logf("Gateway IP: %v\n", offerPacket.GIAddr().String())
+	test.Logf("Client Mac: %v\n", offerPacket.CHAddr().String())
+
+	requestPacket, err := client.SendRequest(&offerPacket)
+	if err != nil {
+		test.Error("Error Sending Request:" + err.Error())
+	}
+
+	test.Log("--Request Packet--")
+	test.Logf("Client IP : %v\n", requestPacket.CIAddr().String())
+	test.Logf("Your IP   : %v\n", requestPacket.YIAddr().String())
+	test.Logf("Server IP : %v\n", requestPacket.SIAddr().String())
+	test.Logf("Gateway IP: %v\n", requestPacket.GIAddr().String())
+	test.Logf("Client Mac: %v\n", requestPacket.CHAddr().String())
+
+	acknowledgement, err := client.GetAcknowledgement(&requestPacket)
+	if err != nil {
+		test.Error("Error Getting Acknowledgement:" + err.Error())
+	}
+
+	test.Log("--Acknowledgement Packet--")
+	test.Logf("Client IP : %v\n", acknowledgement.CIAddr().String())
+	test.Logf("Your IP   : %v\n", acknowledgement.YIAddr().String())
+	test.Logf("Server IP : %v\n", acknowledgement.SIAddr().String())
+	test.Logf("Gateway IP: %v\n", acknowledgement.GIAddr().String())
+	test.Logf("Client Mac: %v\n", acknowledgement.CHAddr().String())
+
+	acknowledgementOptions := acknowledgement.ParseOptions()
+	if dhcp4.MessageType(acknowledgementOptions[dhcp4.OptionDHCPMessageType][0]) != dhcp4.ACK {
+		test.Error("Didn't get ACK?:" + err.Error())
+	}
 }
